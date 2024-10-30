95e78f805cb86fc5f68a3034ce346527b282ff76
CLEOCIMAR GOMES DA SILVA <cleocimar@users.noreply.github.com>
2024-10-30 01:41:22 -0400

    Commitar e exectar para verificar se quebrou o código

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+
+// provideAnimationsAsync('noop') desabilta animações no projeto.
+// Animações podem deixar o projeto lento
+// Passo (9):
+//    Selecionar que o pacote tenha animações mas que fique desabilitado:
+//    Include the Angular animations module? Include, but disable animations
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideAnimationsAsync('noop'),
+    provideHttpClient()
+  ]
 };
